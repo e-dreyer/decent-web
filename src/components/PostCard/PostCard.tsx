242d0c5177d1497815c586a85a2090079860642c
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-07 13:41:57 +0200

    Removed unused import and added Blog

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Box, CardContent, Typography } from "@mui/material";
+import { Box, CardContent, Typography, Stack } from "@mui/material";
 
 import CardWrapper from "../CardWrapper/CardWrapper";
 
@@ -27,16 +27,29 @@ function PostCard(props: PostCardProps) {
             </Link>
           </Box>
 
-          <Link href={`/profiles/${props.post.author?.id}`} passHref>
-            <Typography
-              variant="subtitle2"
-              component="a"
-              color={"primary"}
-              sx={{ textDecoration: "none" }}
-            >
-              {props.post.author?.username}
-            </Typography>
-          </Link>
+          <Stack direction="row" gap={1}>
+            <Link href={`/profiles/${props.post.author?.id}`} passHref>
+              <Typography
+                variant="subtitle2"
+                component="a"
+                color={"primary"}
+                sx={{ textDecoration: "none" }}
+              >
+                {props.post.author?.username}
+              </Typography>
+            </Link>
+
+            <Link href={`/blogs/${props.post.blog?.id}`} passHref>
+              <Typography
+                variant="subtitle2"
+                component="a"
+                color={"primary"}
+                sx={{ textDecoration: "none" }}
+              >
+                {props.post.blog?.name}
+              </Typography>
+            </Link>
+          </Stack>
 
           <Typography variant="body1" component="p">
             {props.post.content}
