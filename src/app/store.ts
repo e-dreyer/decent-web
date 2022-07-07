036d178fc8bebfc5e9324d946a6278d7a4ef5c6a
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-07 11:05:25 +0200

    removed RTK debug mode in production

--- src/app/store.ts
+++ src/app/store.ts
@@ -31,4 +31,4 @@ export type AppStore = ReturnType<typeof makeStore>
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
 
-export const wrapper = createWrapper<AppStore>(makeStore, { debug: true });
+export const wrapper = createWrapper<AppStore>(makeStore, { debug: process.env.NODE_ENV !== 'production' });
