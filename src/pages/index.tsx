ae270478fd28107b52a642a8922bad295c8ff37e
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-08 17:26:08 +0200

    removed old home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,8 @@ import { useGetAllUsersQuery } from "../services/users";
 
 const Home: NextPage = () => {
   const usersQuery = useGetAllUsersQuery();
-  
-  return (
-    <>
-      {usersQuery.isLoading ? (
-        <div>Loading Users</div>
-      ) : usersQuery.error ? (
-        <div>Error loading users</div>
-      ) : usersQuery.data ? (
-        usersQuery.data.map((user, userIndex) => {
-          return <div key={userIndex}>{user.id}</div>;
-        })
-      ) : null}
-    </>
-  );
+
+  return <>Home Page</>;
 };
 
 export default Home;
