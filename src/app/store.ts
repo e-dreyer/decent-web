e292089f1d68d253bf573f1a072a9738de082bdb
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-14 12:37:36 +0200

    depricated Profile

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { createWrapper } from 'next-redux-wrapper';
+import { configureStore } from '@reduxjs/toolkit'
+import { createWrapper } from 'next-redux-wrapper'
 
-import { userApi } from '../services/users';
-import { profileApi } from '../services/profiles';
-import { blogApi } from '../services/blogs';
-import { blogPostApi } from '../services/blogPosts';
-import { blogCommentApi } from '../services/blogComments';
+import { userApi } from '../services/users'
+import { blogApi } from '../services/blogs'
+import { blogPostApi } from '../services/blogPosts'
+import { blogCommentApi } from '../services/blogComments'
 
 export const makeStore = () =>
   configureStore({
     reducer: {
       [userApi.reducerPath]: userApi.reducer,
-      [profileApi.reducerPath]: profileApi.reducer,
       [blogApi.reducerPath]: blogApi.reducer,
       [blogPostApi.reducerPath]: blogPostApi.reducer,
       [blogCommentApi.reducerPath]: blogCommentApi.reducer,
@@ -20,16 +18,15 @@ export const makeStore = () =>
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware()
         .concat(userApi.middleware)
-        .concat(profileApi.middleware)
         .concat(blogApi.middleware)
         .concat(blogPostApi.middleware)
         .concat(blogCommentApi.middleware),
-  });
+  })
 
-export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
 
 export const wrapper = createWrapper<AppStore>(makeStore, {
   debug: process.env.NODE_ENV !== 'production',
-});
+})
