7de5da38608fe7d8214447dd1144a48f1e300bbd
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-18 17:58:52 +0200

    added conditional webpack

--- next.config.js
+++ next.config.js
@@ -1,18 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-      env: {
-        GITHUB_ID: process.env.GITHUB_ID,
-        GITHUB_SECRET: process.env.GITHUB_SECRET,
-      },
-    }
-    return config
-  },
+if (process.env.NODE_ENV === "development") {
 
-  reactStrictMode: true,
+  const nextConfig = {
+    webpackDevMiddleware: config => {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+        env: {
+          GITHUB_ID: process.env.GITHUB_ID,
+          GITHUB_SECRET: process.env.GITHUB_SECRET,
+        },
+      }
+      return config
+    },
+
+    reactStrictMode: true,
+  }
+
+  module.exports = nextConfig
 }
 
-module.exports = nextConfig
+if (process.env.NODE_ENV === "production") {
+
+  const nextConfig = {
+    reactStrictMode: true,
+  }
+
+  module.exports = nextConfig
+}
