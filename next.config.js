8e406110ca1d4590719b3febfe6987ebb1261496
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-18 18:37:38 +0200

    removed conditional webpack setup

--- next.config.js
+++ next.config.js
@@ -1,30 +1,14 @@
 /** @type {import('next').NextConfig} */
-if (process.env.NODE_ENV === "development") {
+const nextConfig = {
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 
-  const nextConfig = {
-    webpackDevMiddleware: config => {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-        env: {
-          GITHUB_ID: process.env.GITHUB_ID,
-          GITHUB_SECRET: process.env.GITHUB_SECRET,
-        },
-      }
-      return config
-    },
-
-    reactStrictMode: true,
-  }
-
-  module.exports = nextConfig
+  reactStrictMode: true,
 }
 
-if (process.env.NODE_ENV === "production") {
-
-  const nextConfig = {
-    reactStrictMode: true,
-  }
-
-  module.exports = nextConfig
-}
+module.exports = nextConfig
