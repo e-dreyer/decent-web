9d47c886be7ec886dae73f0373e247a5f9ebafb3
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-21 11:45:55 +0200

    added autocomplete component

---
+++ src/components/BlogAutocomplete/BlogAutocomplete.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { TextField, Autocomplete } from '@mui/material'
+import { useGetBlogsByUserIdQuery } from '../../services/blogs'
+
+type BlogAutocompleteProps = {
+    userId: string
+    parentCallback: any 
+}
+
+const BlogAutocomplete = (props: BlogAutocompleteProps) => {
+   const blogsQueryResult = useGetBlogsByUserIdQuery({id: props.userId}) 
+
+   const blogs = () => {
+    /* If the query is still loading */
+    if (blogsQueryResult.isLoading) {
+        return []
+    }
+
+    /* If there is an error with the query */
+    if (blogsQueryResult.error) {
+        return []
+    }
+
+    /* If the query is successful and the blogs are available */
+    if (blogsQueryResult.data) {
+        return blogsQueryResult.data.map((blog, blogIndex) => {
+            return { label: blog.name, blogId: blog.id }
+        })
+    }
+
+    /* Otherwise always return an empty array */
+    return []
+   }
+
+   return (
+        <Autocomplete
+            id="blogAutocomplete"
+            options={blogs()}
+            isOptionEqualToValue={(option, value) => option.label === value.label}
+            renderInput={(params) => <TextField {...params} label="Blogs" />}
+            onChange={(e, value) => props.parentCallback(value?.blogId || '')}
+        />
+   )
+}
+
+export default BlogAutocomplete
