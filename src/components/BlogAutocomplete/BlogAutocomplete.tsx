0a5165c342403e96e5477e62699da409ffa547c9
Erik Dreyer <erik.dreyer@protonmail.com>
2022-07-21 11:49:40 +0200

    added type for parentCallback

--- src/components/BlogAutocomplete/BlogAutocomplete.tsx
+++ src/components/BlogAutocomplete/BlogAutocomplete.tsx
@@ -3,44 +3,44 @@ import { TextField, Autocomplete } from '@mui/material'
 import { useGetBlogsByUserIdQuery } from '../../services/blogs'
 
 type BlogAutocompleteProps = {
-    userId: string
-    parentCallback: any 
+  userId: string
+  parentCallback: (blogId: string) => void
 }
 
 const BlogAutocomplete = (props: BlogAutocompleteProps) => {
-   const blogsQueryResult = useGetBlogsByUserIdQuery({id: props.userId}) 
+  const blogsQueryResult = useGetBlogsByUserIdQuery({ id: props.userId })
 
-   const blogs = () => {
+  const blogs = () => {
     /* If the query is still loading */
     if (blogsQueryResult.isLoading) {
-        return []
+      return []
     }
 
     /* If there is an error with the query */
     if (blogsQueryResult.error) {
-        return []
+      return []
     }
 
     /* If the query is successful and the blogs are available */
     if (blogsQueryResult.data) {
-        return blogsQueryResult.data.map((blog, blogIndex) => {
-            return { label: blog.name, blogId: blog.id }
-        })
+      return blogsQueryResult.data.map((blog) => {
+        return { label: blog.name, blogId: blog.id }
+      })
     }
 
     /* Otherwise always return an empty array */
     return []
-   }
+  }
 
-   return (
-        <Autocomplete
-            id="blogAutocomplete"
-            options={blogs()}
-            isOptionEqualToValue={(option, value) => option.label === value.label}
-            renderInput={(params) => <TextField {...params} label="Blogs" />}
-            onChange={(e, value) => props.parentCallback(value?.blogId || '')}
-        />
-   )
+  return (
+    <Autocomplete
+      id="blogAutocomplete"
+      options={blogs()}
+      isOptionEqualToValue={(option, value) => option.label === value.label}
+      renderInput={(params) => <TextField {...params} label="Blogs" />}
+      onChange={(e, value) => props.parentCallback(value?.blogId || '')}
+    />
+  )
 }
 
 export default BlogAutocomplete
